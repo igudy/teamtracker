91fdb0eb2b1cc9c67733bfb4216a7c9e07e88491
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-07-17 11:17:24 +0100

    Connected to mongoDB

--- server/db/connect.js
+++ server/db/connect.js
@@ -4,8 +4,6 @@ const connectDB = async (url) => {
   try {
     await mongoose.connect(url, {
       useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
       useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB');

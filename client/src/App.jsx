e505213f9358a31a0d2e80ef21502332ea11941a
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-07-18 14:05:11 +0100

    Registration startup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react"
 import Login from "./pages/Login"
+import Register from "./pages/Register"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <>
-      <div className="main">
-        <Login />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 

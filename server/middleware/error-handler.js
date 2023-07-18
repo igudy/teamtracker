7ad885a5a4c26da733af43e9f51cdde9125543c3
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-07-18 02:23:52 +0100

    Models and Route folder added

---
+++ server/middleware/error-handler.js
@@ -0,0 +1,11 @@
+import { CustomAPIError } from '../errors.js';
+import { StatusCodes } from 'http-status-codes';
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ msg: err.message });
+  }
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+};
+
+export default errorHandlerMiddleware;

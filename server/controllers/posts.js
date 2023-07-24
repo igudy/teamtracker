5c9b024122eb91c734e800e28ce1609cc062788c
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-07-25 00:05:29 +0100

    Extra security added + Post CRUD API configured

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,36 +1,113 @@
 import Post from "../models/Post.js";
-import { StatusCodes } from "http-status-codes";
+import {
+    StatusCodes
+} from "http-status-codes";
 import NotFoundError from '../errors/index.js'
 import BadRequestError from '../errors/index.js'
 
 const getAllPosts = async (req, res) => {
-    res.send("Get all Posts")
+    const posts = await Post.find({
+        createdBy: req.employee.employeeId
+    }).sort('CreatedAt');
+    res.status(StatusCodes.OK).json({
+        posts,
+        count: posts.length
+    })
 }
 
 const getPost = async (req, res) => {
-    res.send("Get Post")
+    const {
+        employee: {
+            employeeId
+        },
+        params: {
+            id: postId
+        },
+    } = req
+    console.log(req.params.id)
+
+    const post = await Post.findOne({
+        _id: postId,
+        createdBy: employeeId,
+    })
+
+    console.log(post)
+    if (!post) {
+        throw new NotFoundError(`No post with id ${postId}`)
+    }
+    res.status(StatusCodes.OK).json({
+        post
+    })
+    //   res.send('Get Post')
 }
 
 const createPost = async (req, res) => {
     req.body.createdBy = req.employee.employeeId
-    console.log(req.employee.employeeId);
-  const post = await Post.create(req.body)
-  res.status(StatusCodes.CREATED).json({ post })
+    const post = await Post.create(req.body)
+    res.status(StatusCodes.CREATED).json({
+        post
+    })
 }
 
-// const createPost = async (req, res) => {
-//     req.body.createdBy = req.employee.employeeId
-//     console.log(req.body.createdBy)
-//     const post = await Post.create(req.body)
-//     res.status(StatusCodes.CREATED).json({post})
-// }
-
 const updatePost = async (req, res) => {
-    res.send("Update Post")
+    const {
+        body: {
+            title,
+            description,
+            tags
+        },
+        employee: {
+            employeeId
+        },
+        params: {
+            id: postId
+        },
+    } = req
+
+    if (title === '' || description === '' || tags === "") {
+        throw new BadRequestError('Title, description or tags fields cannot be empty')
+    }
+    const post = await Post.findByIdAndUpdate({
+            _id: postId,
+            createdBy: employeeId
+        },
+        req.body, {
+            new: true,
+            runValidators: true
+        }
+    )
+    if (!post) {
+        throw new NotFoundError(`No post with id ${postId}`)
+    }
+    res.status(StatusCodes.OK).json({
+        post
+    })
 }
 
 const deletePost = async (req, res) => {
-    res.send("Delete Post")
+    const {
+        employee: {
+            employeeId
+        },
+        params: {
+            id: postId
+        },
+    } = req
+
+    const post = await Post.findByIdAndRemove({
+        _id: postId,
+        createdBy: employeeId,
+    })
+    if (!post) {
+        throw new NotFoundError(`No post with id ${postId}`)
+    }
+    res.status(StatusCodes.OK).send()
 }
 
-export { getAllPosts, getPost, createPost, updatePost, deletePost }
+export {
+    getAllPosts,
+    getPost,
+    createPost,
+    updatePost,
+    deletePost
+}
--- server/server.js
+++ server/server.js
@@ -21,8 +21,24 @@ import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
 // extra packages
+import helmet from 'helmet';
+import cors from 'cors';
+import xss from 'xss-clean';
+import rateLimit from 'express-rate-limit'; //The library is express-rate-limit not express-rate-limiter
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
 
 // routes
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/posts', authenticateEmployee, postRouter)
 
